deab37068eeccb308c1c535da7993219d7d0a880
Jakob Kristensen <jakob@jakobk.dk>
2020-05-07 14:02:41 +0200

    Fixed bug where begin and end dates were wrongly compared

--- src/originexample/common/models.py
+++ src/originexample/common/models.py
@@ -27,7 +27,7 @@ class DateRange:
 
     @validates_schema
     def validate_begin_before_end(self, data, **kwargs):
-        if data['begin'] < data['end']:
+        if data['begin'] > data['end']:
             raise ValidationError({
                 'begin': ['Must be before end'],
                 'end': ['Must be after begin'],
@@ -48,7 +48,7 @@ class DateTimeRange:
 
     @validates_schema
     def validate_begin_before_end(self, data, **kwargs):
-        if data['begin'] < data['end']:
+        if data['begin'] > data['end']:
             raise ValidationError({
                 'begin': ['Must be before end'],
                 'end': ['Must be after begin'],
