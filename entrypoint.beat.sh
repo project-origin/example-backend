c359ac466d1f9ad061f0d4a9af175d4e3d37fa49
Jakob Kristensen <jakob@jakobk.dk>
2020-07-03 10:46:52 +0200

    Fixed bug where containers performed COLD shutdown every time

--- entrypoint.beat.sh
+++ entrypoint.beat.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 cd /app/migrations && pipenv run migrate || exit
-cd /app && pipenv run celery beat -A originexample.pipelines -l info
+cd /app && exec pipenv run celery beat -A originexample.pipelines -l info
--- entrypoint.worker.sh
+++ entrypoint.worker.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 cd /app/migrations && pipenv run migrate || exit
-cd /app && pipenv run celery worker -A originexample.pipelines -O fair -l info --pool=gevent --concurrency=$CONCURRENCY
+cd /app && exec pipenv run celery worker -A originexample.pipelines -O fair -l info --pool=gevent --concurrency=$CONCURRENCY
